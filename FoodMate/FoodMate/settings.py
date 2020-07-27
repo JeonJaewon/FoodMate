5082ab38a5ac35a4c431b685ef055a711873d78f
jiwoo <hjw6112@konkuk.ac.kr>
2020-07-28 01:28:57 +0900

    add photo model

--- FoodMate/FoodMate/settings.py
+++ FoodMate/FoodMate/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'map',
+    'photo',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -55,7 +57,7 @@ ROOT_URLCONF = 'FoodMate.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'layout')], #base.html 탬플릿 경로 지정
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -104,9 +106,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ko-kr'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
@@ -119,3 +121,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),  # Root의 static 파일
+)
+
+#이미지가 media 폴더에 지속적으로 저장되도록
+MEDIA_URL = '/res/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
---
+++ FoodMate/photo/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from .models import Photo, Image
+
+admin.site.register(Photo)
+admin.site.register(Image)
